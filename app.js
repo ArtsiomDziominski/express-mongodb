bc5988aa22914f2544ee8bfc58e502d608098625
Artsiom Dziominski <artsiomdziominski@gmail.com>
2023-01-02 20:04:29 +0400

    get request

--- app.js
+++ app.js
@@ -2,4 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/post', (req,res)=> {
+    res.status(200).json()
+})
+
+app.get('/', function (req, res) {
+    res.send('Create Post\n Artsiom Dziominski')
+})
+
 app.listen(PORT, () => console.log('Server started on port ', PORT))
