79ae78d34982f9ff728c289fa0cca9d18f94d44e
Artsiom Dziominski <artsiomdziominski@gmail.com>
2023-01-02 23:51:00 +0400

    save post

---
+++ models/post.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+}, {timestamps: true})
+
+const Post = mongoose.model('Post', postSchema)
+
+module.exports = Post;
